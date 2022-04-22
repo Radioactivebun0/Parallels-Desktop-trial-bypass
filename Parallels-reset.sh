ad3b9883df91fb88ea5e56263eacfae2cb4d5b83
Radioactivebun0 <70781995+Radioactivebun0@users.noreply.github.com>
2022-04-22 06:35:13 -0700

    Add files via upload

---
+++ Parallels-reset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+rm -rfd ~/library/preferences/com.parallels*
+rm -rfd ~/library/preferences/parallels/*
+rm -rfd ~/library/preferences/parallels
+rm -rfd /private/var/db/parallels/stats/*
+rm -rfd /private/var/db/parallels/stats
+rm -rfd /private/var/db/parallels
+rm -rfd /library/logs/parallels.log
+rm -rfd /library/preferences/parallels/*
+rm -rfd /library/preferences/parallels
+rm -rfd /private/var/db/parallels
+rm -rfd /private/var/.parallels_swap
+rm -rfd /private/var/db/receipts/'com.parallels*'
+rm -rfd /private/tmp/qtsingleapp-*-lockfile
+rm -rfd /private/tmp/com.apple.installer*/*
+rm -rfd /private/tmp/com.apple.installer*
+rm -rfd /private/var/root/library/preferences/com.parallels.desktop.plist
+rm -fr ~/Library/Logs/parallels.log
+rm -fr /Library/Preferences/Parallels/
+rm -fr ~/Library/Preferences/Parallels
+rm -fr /private/var/.Parallels_swap
+rm -fr /private/var/db/Parallels/Stats
+rm -fr /private/tmp/qtsingleapp-Parall-c0ce-0-lockfile
+rm -fr ~/Library/Preferences/com.parallels
